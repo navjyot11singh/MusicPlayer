7abad2d28a2ba0836161dc2ed9641e71beeea2a9
Navjyot singh <navjyot.marwah@gmail.com>
2019-01-08 03:40:21 +0530

    Initial commit

---
+++ app/src/main/java/androidsingh/navjyotsingh/andromusic/MainActivity.java
@@ -0,0 +1,272 @@
+package androidsingh.navjyotsingh.andromusic;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<SongInfo> _songs = new ArrayList<SongInfo>();;
+    RecyclerView recyclerView;
+    SongAdapter songAdapter;
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+    Button playPauseButton;
+     boolean isPlaying = false;
+    private Handler myHandler = new Handler();
+    SeekBar timeline;
+
+
+
+    public void playMe(){
+        mediaPlayer.start();
+    }
+    public void pauseMe(){
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recyclerView);
+        songAdapter = new SongAdapter(this,_songs);
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.testaudio);
+
+        audioManager=(AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+
+        playPauseButton=findViewById(R.id.buttonPlayPause);
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying==false){
+                    playMe();
+                    isPlaying=true;
+                    playPauseButton.setBackgroundResource(R.drawable.pause);
+
+                }
+                else{
+                    pauseMe();
+                    isPlaying=false;
+                    playPauseButton.setBackgroundResource(R.drawable.play);
+                }
+            }
+        });
+
+
+
+        timeline=findViewById(R.id.timeline);
+        timeline.setMax(mediaPlayer.getDuration());
+
+//        timeline.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//               if (fromUser)
+//                   mediaPlayer.seekTo(progress);
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//        });
+
+
+//        new Timer().scheduleAtFixedRate(new TimerTask() {
+//            @Override
+//            public void run() {
+//                timeline.setProgress(mediaPlayer.getCurrentPosition());
+//            }
+//        }, 0, 1000);
+
+
+
+        recyclerView.setAdapter(songAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                linearLayoutManager.getOrientation());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        songAdapter.setOnItemClickListener(new SongAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(final Button b, View view, final SongInfo obj, int position) {
+                if(b.getBackground().equals(R.drawable.pause)){
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                    b.setBackgroundResource(R.drawable.play);
+                    isPlaying = false;
+                    playPauseButton.setBackgroundResource(R.drawable.play);
+
+                }else {
+
+                    Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                mediaPlayer = new MediaPlayer();
+                                mediaPlayer.setDataSource(obj.getSongUrl());
+                                mediaPlayer.prepareAsync();
+                                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                                    @Override
+                                    public void onPrepared(MediaPlayer mp) {
+                                        mp.start();
+                                        timeline.setProgress(0);
+                                        timeline.setMax(mediaPlayer.getDuration());
+                                        timeline.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                                            @Override
+                                            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                                                if (fromUser)
+                                                    mediaPlayer.seekTo(progress);
+                                            }
+
+                                            @Override
+                                            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                                            }
+
+                                            @Override
+                                            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                                            }
+                                        });
+                                        Log.d("Prog", "run: " + mediaPlayer.getDuration());
+                                        isPlaying = true;
+                                        playPauseButton.setBackgroundResource(R.drawable.pause);
+
+                                    }
+                                });
+                                b.setBackgroundResource(R.drawable.pause);
+
+
+
+                            }catch (Exception e){}
+                        }
+
+                    };
+                    myHandler.postDelayed(runnable,100);
+
+                }
+            }
+        });
+        checkUserPermission();
+
+        Thread t = new runThread();
+        t.start();
+
+    }
+    public class runThread extends Thread {
+
+
+        @Override
+        public void run() {
+            while (true) {
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("Runwa", "run: " + 1);
+                if (mediaPlayer != null) {
+                    timeline.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeline.setProgress(mediaPlayer.getCurrentPosition());
+                        }
+                    });
+
+                    Log.d("Runwa", "run: " + mediaPlayer.getCurrentPosition());
+                }
+            }
+        }
+
+    }
+
+    private void checkUserPermission(){
+        if(Build.VERSION.SDK_INT>=23){
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
+                return;
+            }
+        }
+        loadSongs();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 123:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    loadSongs();
+                }else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    checkUserPermission();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        }
+
+    }
+
+    private void loadSongs(){
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC+"!=0";
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if(cursor != null){
+            if(cursor.moveToFirst()){
+                do{
+                    String name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                    String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                    String url = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+
+                    SongInfo s = new SongInfo(name,artist,url);
+                    _songs.add(s);
+
+                }while (cursor.moveToNext());
+            }
+
+            cursor.close();
+            songAdapter = new SongAdapter(MainActivity.this,_songs);
+
+        }
+    }
+}
